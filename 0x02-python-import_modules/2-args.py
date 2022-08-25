4f3d3cb497cfc96a99fc93e15e3dde6525fc7b0a
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-08-25 15:23:30 -0700

    python project on argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__  == "__main__":
+    import sys
+    usr = sys.argv[1:]
+    length = len(usr)
+    i = 1
+    if length == 0:
+        print("{} arguments.".format(length))
+    elif length == 1:
+        print("{} argument:".format(length))
+        print("{}: {}".format(length, usr[0]))
+    else:
+        print("{} arguments:".format(length))
+        for word in usr:
+            print("{}: {}".format((i), word))
+            i += 1
