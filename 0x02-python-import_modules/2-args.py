62847719a98b67da25fc71218ac41f996266211e
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-08-25 15:25:17 -0700

    python project on argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__  == "__main__":
-    import sys
-    usr = sys.argv[1:]
+    from sys import argv
+    usr = argv[1:]
     length = len(usr)
     i = 1
     if length == 0:
