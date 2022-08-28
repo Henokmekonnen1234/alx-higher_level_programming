24015935c56267288639c9013f0089e718a07f50
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-08-28 15:00:32 -0700

    python project on find the max integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    temp = 0
     if my_list is not None:
+        temp = 0
         for i in my_list:
             if temp < i:
                 temp = i
