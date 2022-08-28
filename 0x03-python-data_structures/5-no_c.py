677ed071118f9a3ff28b7a74dc05066bccb30cc8
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-08-28 11:31:04 -0700

    project on remove c and C in python

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def n0_c(my_string):
+def no_c(my_string):
     if my_string is not None:
+        my_string = list(my_string)
         for ind in range(len(my_string)):
-            if ord('c') == ord(my_string[ind]) and ord('C') == ord(my_string[ind]):
+            if ord('c') == ord(my_string[ind]) or ord('C') == ord(my_string[ind]): 
                 my_string[ind] = ''
+        my_string=''.join(my_string)
         return my_string
