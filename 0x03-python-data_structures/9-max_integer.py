4b0b455800cc10b8ea80a4041c9539d53d989d9f
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-08-28 15:03:22 -0700

    python project on find the max integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
-        return None
-    else:
-        temp = 0
+    if my_list:
+        biggest = my_list[0]
         for i in my_list:
-            if temp < i:
-                temp = i
-        return temp
+            if i > biggest:
+                biggest = i
+        return biggest
+    else:
+        return None
