26c1284f8cc684738f4fb099d5d10671f362639e
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-08-31 15:57:00 -0700

    sort dictionary using key

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary is not None:
-        for keys in sorted(a_dictionary.key()):
-            print("{}: {}".format(keys, a_dictionary.key(keys)))
+        for keys in sorted(a_dictionary.keys()):
+            print("{}: {}".format(keys, a_dictionary[keys]))
