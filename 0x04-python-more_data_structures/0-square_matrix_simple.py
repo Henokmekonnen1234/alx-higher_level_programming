a96369beb7122a51178d2bea5afa2d40cc87faf7
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-08-31 04:55:52 -0700

    project on square of matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     if matrix is not None:
         matrix2 = matrix.copy()
-        matrix2 = [i**2 for i in matrix2]
+        matrix2 = [[row[i] * row[i] for row in matrix2] for i in range(3)]
         return matrix2
