7836c30d7bb43fc6bf7dfba1f06eeb998fb88bc5
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-08-31 14:56:19 -0700

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
+    if search is None or replace is None or my_list is None:
+        return my_list
+    else:
         return list(map(lambda x: replace if x == search else x, my_list))
