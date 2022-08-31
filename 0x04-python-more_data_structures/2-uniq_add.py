d021793ab97ee7fb8ab71b52d9a4eb788e9e9d7f
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-08-31 15:14:28 -0700

    add only uniq value

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    sum = 0
-    return (map(lambda x: sum += x, set(my_list))) 
+    if my_list:
+        sum = 0
+        for i in set(my_list):
+            sum += i
+        return sum 
