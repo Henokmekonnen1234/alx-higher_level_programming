58d3cb1a938d577421c9fe3f7d21e93b96cbeb2d
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-09-13 14:27:08 -0700

    Readme files

--- 0x05-python-exceptions/0-safe_print_list.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    if my_list is not None and x is not None:
-        try:
-            x -= 1
-            return (my_list[x])
-        except IndexError:
-            return (my_list[-1])
--- 0x05-python-exceptions/1-safe_print_integer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        if int(value):
-            print("{:d}".format(value))
-            return True
-    except (TypeError, ValueError):
-        return False
