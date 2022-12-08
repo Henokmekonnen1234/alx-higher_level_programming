11f2fe73be1e5be6060d30ceb53041e829d614a9
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-12-08 01:41:45 -0800

    filter the N word and print them

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,14 +4,11 @@ import MySQLdb
 import sys
 
 lists = sys.argv
-
+sql = "SELECT * FROM states WHERE name like 'N%'"
 if __name__ == '__main__':
     db = MySQLdb.connect("localhost", lists[1], lists[2], lists[3])
-    
     cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states")
-
+    cursor.execute(sql)
     results = cursor.fetchall()
-    for row in results:
-        print(row)
+    for rows in results:
+        print(rows)
