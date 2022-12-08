ff932bd94c3eef96532a0784f36e5db2b8f0c84a
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-12-08 02:41:50 -0800

    python tasks

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,15 +4,11 @@ import MySQLdb
 import sys
 
 lists = sys.argv
-
+sql = "SELECT * FROM states WHERE name LIKE '%s'" % (lists[4])
 if __name__ == '__main__':
     db = MySQLdb.connect("localhost", lists[1], lists[2], lists[3])
-    
     cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states")
-
+    cursor.execute(sql)
     results = cursor.fetchall()
-    for row in results:
-        print(row)
-db.close()
+    for rows in results:
+        print(rows)
