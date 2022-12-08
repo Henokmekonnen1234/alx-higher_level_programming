e4df42c7fbcadd019b16fc6f89a10635c48411f2
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-12-08 01:10:40 -0800

    list and sort them all

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""
-Lists all states from database hbtn_0e_0_usa.
-Usage: ./0-select_states.py <mysql username> \
-                            <mysql password> \
-                            <database name>
-"""
-import sys
-import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+import MySQLdb
+import sys
+
+lists = sys.argv
+
+if __name__ == "main":
+    db = MySQLdb.connect("localhost", lists[1], lists[2], lists[3])
+    
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states")
+
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
