7f989f51bf3d20ddea019d158b623596a49574c6
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-08-11 00:19:08 +0300

    print all letter in uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for string in str:
         if ord(string) >= 97 and ord(string) <= 122:
             string = chr((ord(string) - 32))
-        print(string, end="")
+        print("{}".format(string), end="")
     print()
