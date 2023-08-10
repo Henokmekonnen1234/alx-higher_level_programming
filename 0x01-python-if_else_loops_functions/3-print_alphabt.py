cdb8a3b4ed50afb2c30c7e4cc0b634cbd57d0e9a
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-08-11 01:19:03 +0300

    print all lower case alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('a'),  ord('z') + 1):
-    print("{}".format(chr(i)), end="")
+    if chr(i) == 'q' or chr(i) == 'e':
+        continue
+    else:
+        print("{}".format(chr(i)), end="")
