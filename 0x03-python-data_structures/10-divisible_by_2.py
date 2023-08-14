eb0665087e07413bb065d52ff8c441d08c82569f
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-08-14 19:00:14 +0300

    finds all multiples of 2 in a list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     if my_list:
-        lisdiv = list(my_list)
+        lisdiv = []
         for i in range(len(my_list)):
             if my_list[i] % 2 == 0:
-                lisdiv[i] = True
+                lisdiv.append(True)
             else:
-                lisdiv[i] = False
+                lisdiv.append(False)
     return lisdiv
