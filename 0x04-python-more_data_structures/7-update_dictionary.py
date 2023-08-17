0a243085a82aba0f629f9cd82f80b281db2d8843
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-08-17 18:57:15 +0300

    function that replaces or adds key/value in a dictionary.

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key and type(key) is str and key in a_dictionary.keys():
+    if key in a_dictionary.keys():
         a_dictionary[key] = value
         return a_dictionary
     else:
-        a_dictionary[key]  = value
+        a_dictionary[key] = value
         return a_dictionary
