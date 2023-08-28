4f27bfb7b8e0c00dcb5f64bc1a748f7a0b124555
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-08-29 00:45:10 +0300

    function that prints an integer.

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as TE:
+    except (TypeError, ValueError) as TE:
         sys.stderr.write("Exception: " + str(TE) + '\n' )
         return False
