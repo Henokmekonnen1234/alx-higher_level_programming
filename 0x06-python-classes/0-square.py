5f0001d87ebb8a4968cf5ab9feea6061be35f9bd
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-08-29 20:52:01 +0300

    empty class file

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" It's Square class that defines nothing for now
+           but some methods will be added to it"""
+
+
 class Square:
     """ It's Square class that defines nothing for now
         but some methods will be added to it"""
