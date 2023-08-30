71dec75616d298833e2969ef888d4d39db45692e
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-08-30 12:47:15 +0300

    task 4 square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -41,3 +41,11 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         selfValueError("size must be >= 0")
+
+    def area(self):
+        """Calculate the are of square
+
+        Return:
+            int: the square of the sides
+        """
+        return self.__size ** 2
