7d6b86dbab0870840b15f3e49a267d7e0cbe42b7
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-08-30 10:58:19 +0300

    class Square that defines a square and have private instace size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!usr/bin/python3
+""" Square class is a class which have size attribute
+    with private instance attribute """
+
+
+class Square:
+
+    def __init__(self, size):
+
+        """ the __init__ method is special type of method
+            which it intialize the fields of the class
+
+            Attributes
+            ----------
+            size : int
+                it holds the length of the values
+            Return
+            ------
+            no value to return """
+        self.__size = size
