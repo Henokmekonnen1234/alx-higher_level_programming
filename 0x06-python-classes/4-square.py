7600a22f274eb6a8fb2c7d2fcbddfed0dd4d4140
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-08-30 12:41:22 +0300

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Module 4-square.py
+
+This Module defies the Square class
+"""
+
+class Square:
+    """this class define about the square class"""
+
+    def __init__(self, size=0):
+        """intialaize the size instance
+        
+        Args:
+            size (int, optional): length of the sizes
+        """
+        self.__size == size
+
+    @property
+    def size(self):
+        """Getter method
+
+        Return:
+            int: the value of size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method 
+
+        Args:
+            value (int, optional): int value to  set size
+
+        Raise:
+            TypeError: if size is not integer
+            ValueError: if size < 0
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        selfValueError("size must be >= 0")
