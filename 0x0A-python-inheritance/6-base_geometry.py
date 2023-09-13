f09b8a4e8538e00ed9d2303e50009a25432339a4
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-09-13 12:09:08 -0700

    add more methods

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry.py
+
+This module will be described below
+"""
+
+
+class BaseGeometry:
+    """This class contain usefull methods"""
+
+    def __init__(self):
+        """empty intialization"""
+        pass
+
+    def area(self):
+        """ unimplamented method
+
+        Raises:
+            Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
