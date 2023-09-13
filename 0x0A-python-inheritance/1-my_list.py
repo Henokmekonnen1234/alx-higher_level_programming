20f20b9eaaf75a4aaec23c681afc3a31633722c6
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-09-13 20:25:36 +0300

    second task create myList which inherit list class

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module 1-my_list.py
+
+This module will be described below
+"""
+
+
+class MyList(list):
+    """ This class inherit list from list class """
+
+    def __init__(self):
+        """initialation method 
+
+        Args:
+            value (int): empty list
+            count (int): numbers of elements in list
+        """
+        self.value = []
+        
+    def append(self, number):
+        """ this method add numbers to the list
+
+        Args:
+            number (int): numbers passed to class 
+                          tobe added to the list
+        """
+        self.value.append(number)
+
+    def print_sorted(self):
+        """this method will sort the value list
+            in accending order
+        """
+        print(sorted(list(self.value)))
+
+    def __str__(self):
+        """This will return list when the class called"""
+        return f"{self.value}"
