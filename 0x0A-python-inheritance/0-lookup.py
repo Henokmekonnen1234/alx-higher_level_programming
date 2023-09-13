6f992f641459adee5345716925bd6db9a8903949
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-09-13 13:24:24 +0300

    the first task dir() concept

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module 0-lookup.py
+
+This module will be explained in below file
+"""
+
+
+def lookup(obj):
+    """ this function accepts object
+
+    Args:
+        obj (object): object attribute
+
+    Returns:
+        list: list of fields and objects
+    """
+    return dir(obj)
