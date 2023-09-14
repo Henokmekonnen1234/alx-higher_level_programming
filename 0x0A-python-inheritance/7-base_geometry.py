10d1d23da6a440b8daa74d097e762044bc0f17ee
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-09-14 14:09:37 +0300

    task 7 with tests

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module 7-base_geometry.py
+
+This module will be described below
+"""
+
+
+class BaseGeometry:
+    """This class contain usefull methods"""
+
+    def __init__(self):
+        """empty intialization"""
+        pass
+
+    def area(self):
+        """ unimplamented method
+
+        Raises:
+            Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """This method validates value variable
+
+        Args:
+            value (int): integer value
+            name (str): string value
+
+        Raises:
+            TypeError: if value is not integer
+            ValueError: if value is less than 0
+        """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
