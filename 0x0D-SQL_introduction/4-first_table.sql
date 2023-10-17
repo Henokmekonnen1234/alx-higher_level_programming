cba8e3765789d451852eda149a59707c741bd912
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-10-18 00:55:53 +0300

    updated

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,6 @@
 -- create table frist_table
-CREATE TABLE IF NOT EXISTS first_name(
+CREATE TABLE IF NOT EXISTS first_name
+(
 	id INT NOT NULL,
-	name VARCHAR(256) NOT NULL)
+	name VARCHAR(256) NOT NULL
+);
