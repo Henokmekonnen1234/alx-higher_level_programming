acdf022173b087ec251c9f44b6526a4ca314615e
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-10-18 17:20:29 +0300

    updated on task 6

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,3 +1,23 @@
--- This query lists all data in first_name table
-SELECT *
-FROM first_name;
+DELIMITER //
+
+CREATE PROCEDURE checkAndSelectTable()
+BEGIN
+	  DECLARE tableExists INT;
+	  
+	  -- Check if the table exists
+  SELECT 1 INTO tableExists FROM information_schema.TABLES WHERE TABLE_NAME = 'first_name';
+  
+  IF tableExists = 1 THEN
+	    -- Table exists, so select its data
+    SELECT * FROM first_name;
+  ELSE
+	    -- Table does not exist
+    SELECT 'the table first_name does not exist.';
+  END IF;
+END //
+
+DELIMITER ;
+
+-- Call the stored procedure
+CALL checkAndSelectTable();
+
