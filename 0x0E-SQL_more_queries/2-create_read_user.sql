c0ab6a10e6a0075ae25df1090c8fa884766dbf43
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-10-19 13:13:28 +0300

    task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,7 +4,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES
+GRANT USAGE
 ON *.*
 TO 'user_0d_2'@'localhost';
 GRANT SELECT
