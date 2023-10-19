5566c40911b310d973f6743681044108982bfa4f
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-10-19 15:14:54 +0300

    task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,6 @@
 -- this query will create a database hbtn_0d_use and table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
 (
 	id INT UNIQUE AUTO_INCREAMENT NOT NULL PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
