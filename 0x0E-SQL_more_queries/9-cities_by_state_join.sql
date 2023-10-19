c22099f024d46a3a5ff3f0bca620a0a13744f2b6
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-10-19 11:34:08 -0700

    task 10

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,5 @@
--- List all cities in the database hbtn_0d_usa.
--- Records are sorted in order of ascending cities.id.
-SELECT c.`id`, c.`name`, s.`name`
-FROM `cities` AS c
-	INNER JOIN `states` AS s
-	ON c.`state_id` = s.`id`
-ORDER BY c.`id`;
+-- this query will show city id , city name and state name 
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id;
