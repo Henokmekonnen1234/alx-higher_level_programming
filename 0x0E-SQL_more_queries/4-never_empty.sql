e9d181bade5e7a29e95185fb6aefefadd1a91608
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-10-19 14:52:41 +0300

    task 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,6 +1,6 @@
 -- this will create id__not_null table 
 CREATE TABLE IF NOT EXiSTS id_not_null
 (
-	id INT UNIQUE NOT NULL DEFAULT 1 PRIMARY KEY,
+	id INT NOT NULL DEFAULT 1,
 	name VARCHAR(256)
 );
