a79eb67ab89b4e32f29d342c70bf578c530c4b72
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-10-19 15:31:09 +0300

    task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,11 @@
--- Creates the database hbtn_0d_usa with the table cities.
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
-	PRIMARY KEY(`id`),
-	`id`	INT	NOT NULL AUTO_INCREMENT,
-	`state_id`	INT	NOT NULL,
-	`name`	VARCHAR(256)	NOT NULL,
-	FOREIGN KEY(`state_id`)
-	REFERENCES `hbtn_0d_usa`.`states`(`id`)
+-- this query will create hbtn_0d_usa ad create table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities
+(
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	CONSTRAINT fk_state
+	FOREIGN KEY (state_id)
+	REFERECES states(id)
 );
