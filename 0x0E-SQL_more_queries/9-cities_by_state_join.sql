6239f04f5fe59169d5de708af5f1b4f002e3406b
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-10-19 12:04:21 -0700

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- this query will show city id , city name and state name 
 SELECT cities.id, cities.name, states.name
-FROM cities, states
-WHERE cities.state_id = states.id
+FROM cities
+INNER JOIN states
+ON cities.state_id = states.id
 ORDER BY cities.id;
