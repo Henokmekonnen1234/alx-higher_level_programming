6edb54f64b755139a5ab4cc3986b422e2eabf680
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-11-13 22:43:34 +0300

    task 3 completed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,7 @@
 
 const args = process.argv;
 if (args.length <= 2) {
-	console.log('No argument');
-}
-else {
-	console.log('Argument found');
+  console.log('No argument');
+} else {
+  console.log('Argument found');
 }
