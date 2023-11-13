a2a9bb41a847b75f09705c27b7ad2ae5e5ade138
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-11-13 23:01:49 +0300

    task 3 is edited and completed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
-const args = process.argv;
-if (args.length <= 2) {
-  console.log('No argument');
-} else {
+const { argv } = require('node:process');
+const args = argv.slice(2);
+if (args.length > 0) {
   console.log('Argument found');
+} else {
+  console.log('No argument');
 }
