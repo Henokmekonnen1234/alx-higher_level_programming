3a116916f4140d2f51e98ab6823c340d0d076790
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-11-13 23:26:32 +0300

    task 4 completed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,8 +3,8 @@
 const { argv } = require('node:process');
 let length = 0;
 argv.forEach((value, index) => { length += 1; });
-if (length <= 2) {
-  console.log('No Argument');
-} else {
+if (argv[2]) {
   console.log(argv[2]);
+} else {
+  console.log('No Argument');
 }
