f00b16d882e32438bc3c0bfcc640c7b35bdd346e
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-11-24 15:04:16 +0300

    task 8 completed

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
                                       sys.argv[3]), pool_pre_ping=True)
         Base.metadata.create_all(engine)
         session = Session()
-        result = session.query(State).all()
+        result = session.query(State).first()
         if result is not None:
-            print("{}: {}".format(result[0].id, result[0].name))
+            print("{}: {}".format(result.id, result.name))
         else:
             print(Nothing)
     except Exception as e:
