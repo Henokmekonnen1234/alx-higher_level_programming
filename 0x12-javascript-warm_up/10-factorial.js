dfaf7cf841795303050be4406079891b020110ad
Henokmekonnen1234 <henokmac2425@gmail.com>
2024-01-03 19:53:46 +0300

    task 10 completed

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-if (Number(process.argv[2]) === NaN | Number(process.argv[2]) === undefined |
-	Number(process.argv[2]) === 0 | Number(process.argv[2]) === 1) {
+if (isNaN(Number(process.argv[2])) | Number(process.argv[2]) === undefined |
+Number(process.argv[2]) === 0 | Number(process.argv[2]) === 1) {
   console.log(1);
 } else {
   let number = 1;
-  for (let i = 1; i <= Number.parseInt(process.argv[2]); i++){
+  for (let i = 1; i <= Number.parseInt(process.argv[2]); i++) {
     number *= i;
   }
   console.log(number);
