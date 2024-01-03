52e47d8256fe1272ca0695032711b3afa9f32ef3
Henokmekonnen1234 <henokmac2425@gmail.com>
2024-01-03 20:19:45 +0300

    task 10 comleted

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-if (isNaN(Number(process.argv[2])) | Number(process.argv[2]) === undefined |
-Number(process.argv[2]) === 0 | Number(process.argv[2]) === 1) {
-  console.log(1);
-} else {
-  let number = 1;
-  for (let i = 1; i <= Number.parseInt(process.argv[2]); i++) {
-    number *= i;
+const number = function (x) {
+  if (isNaN(x) || x === 1 || x === 0) {
+    return 1;
+  } else {
+    return x * (number(x - 1));
   }
-  console.log(number);
-}
+};
+
+console.log(number(Number.parseInt(process.argv[2])));
