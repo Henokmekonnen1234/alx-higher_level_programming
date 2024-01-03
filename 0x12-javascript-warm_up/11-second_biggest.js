c972a0ebe567b392e574ad2cbd8ae3a326ffab57
Henokmekonnen1234 <henokmac2425@gmail.com>
2024-01-03 22:36:47 +0300

    task 11 comleted

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 
-process.argv.sort();
-if (process.argv.length >= 4) {
-  console.log(Number.parseInt(process.argv[process.argv.length - 2]));
-} else {
-  console.log(0);
-}
+const bigNum = (x) => {
+  if (process.argv.length <= 2) {
+    console.log(0);
+  } else {
+    console.log(process.argv[process.argv.length - 1]);
+  }
+};
+
+process.argv.splice(0, 2);
+bigNum(process.argv.sort());
