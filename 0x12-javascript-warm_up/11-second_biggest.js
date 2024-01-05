65e376f7f91888e1bf12a223cba67760714618e0
Henokmekonnen1234 <henokmac2425@gmail.com>
2024-01-05 15:04:02 +0300

    completed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
-const bigNum = (x) => {
-  if (process.argv.length <= 3) {
-    console.log(0);
-  } else {
-    const x = process.argv.reduce((prev, next) => prev > next ? prev : next, 0);
-    console.log(x);
-  }
-};
-
-bigNum(process.argv);
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+    process.argv.sort();
+    console.log(process.argv[process.argv.length -2]);
+}
