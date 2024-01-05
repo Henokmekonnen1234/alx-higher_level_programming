cb8e6a4d44c5b66a7534863fd9491429726c3029
Henokmekonnen1234 <henokmac2425@gmail.com>
2024-01-05 20:42:53 +0300

    Task completed

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
   width;
   height;
   constructor (w, h) {
     this.width = w;
     this.height = h;
   }
-};
+}
+
+module.exports = Rectangle;
