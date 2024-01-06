2c1805e2f382a5e18612912524e8e0ab4bcdce4c
Henokmekonnen1234 <henokmac2425@gmail.com>
2024-01-06 16:23:08 +0300

    Task completed

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,15 +3,15 @@ const Square1 = require('./5-square');
 class Square extends Square1 {
   constructor (size) {
     super(size);
-    this.size1 = size;
+    this.size = size;
   }
 
   charPrint (c) {
     if (c === undefined || c === null) {
       this.print();
     } else {
-      for (let i = 0; i < this.size; i++) {
-        for (let j = 0; j < this.size; j++) {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
           process.stdout.write(c);
         }
         console.log('');
